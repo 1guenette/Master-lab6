04718b135e93fc8aba40c67913cda64855b4f304
1guenette <guenette@iastate.edu>
2017-04-13 02:09:22 -0500

    GetHistory

--- getHistory.php
+++ getHistory.php
@@ -1,10 +1,27 @@
 <?php
 
 require_once 'config.php';
+session_start()
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
+if ($_SERVER["REQUEST_METHOD"] == "GET") 
 {
-     
+	$name = $GET["username"];
+	$sql = "SELECT * FROM loanHistory WHERE username ='name'"
+	$sql =mysqli_query($conn,$sql);
+	$val = "<ul>";
+
+	for($log = mysql_fetch_array($sql))
+	{
+		$id = $log["bookID"];
+		$date = $log["startDate"];
+
+		val+="<li>ID: $id....Date: $date</li>";
+
+
+	}     
+	$val+="</ul>"
+
+	echo $val;
 }
 
 
--- rent.php
+++ rent.php
@@ -7,22 +7,43 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
 	$bookName = $_GET["bookName"];
 
-	$sql = "SELECT * FROM books WHERE bookName = '$bookName', available=true";
-	 //mysqli_query($conn,"SELECT bookName, rentNum, bookID FROM books WHERE genre = 'sport', available='true'");
-	mysqli_query($conn,$sql);
 
-	$data = "";
+	$sql = "SELECT * FROM books WHERE bookName = '$bookName', available=true";
+	mysqli_query($conn,$bookData);
+	
+
+
+    $data = "";
 	
 	while ($bookData = mysql_fetch_array($sql)) 
     {
         if($_SESSION["rentNum"]<2)
         {
-        	echo"<script>alert('Book has been added to your rental cart')</script>"
+            $_SESSION["rentNum"] +=1;
+            $newRentNum = $_SESSION["rentNum"];
+            
+            $user = $_SESSION["username"];
+            $id= $bookData["bookID"];
+            $startDate = date("Y/m/d");
+            
+
+            if($_SESSION["rentNum"] ==1)
+            {
+                mysqli_query($conn, "INSERT INTO accounts (rentNum, rent1) VALUES '$newRentNum','$bookName");
+                mysqli_query($conn, "INSERT INTO loanHistory (username, startDate, bookID) VALUES '$user',  '$startDate', '$id'");
+            }
+            else
+            {
+                mysqli_query($conn, "INSERT INTO accounts (rentNum, rent2) VALUES '$newRentNum','$bookName");
+                mysqli_query($conn, "INSERT INTO loanHistory (username, startDate) VALUES '$user',  '$startDate'");
+            }
+           
+            $data+="<script>alert('Book has been added to your rental cart')</script>"
 
         }
         else
         {
-        	echo"<script>alert('You have rented too many books')</script>"
+        	$data+="<script>alert('You have rented too many books')</script>"
         }
     }
 
